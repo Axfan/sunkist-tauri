f705891f651d1dd8b7f784fcedde3a71bfdbfac9
Axfan <axfanwashere@gmail.com>
2022-06-05 12:47:45 -0700

    feat: compress images

--- src/bgs/index.js
+++ src/bgs/index.js
@@ -5,22 +5,11 @@ import bg2 from './2.jpg';
 import bg3 from './3.jpg';
 import bg4 from './4.jpg';
 import bg5 from './5.jpg';
-import bg6 from './6.jpg';
+import bg6 from './6.png';
 import bg7 from './7.jpg';
 import bg8 from './8.jpg';
 import bg9 from './9.jpg';
-import bg10 from './10.png';
-import bg11 from './11.png';
-import bg12 from './12.jpg';
-import bg13 from './13.jpg';
-import bg14 from './14.jpg';
-import bg15 from './15.jpg';
-import bg16 from './16.jpg';
-import bg17 from './17.jpg';
-import bg18 from './18.jpg';
-import bg19 from './19.jpg';
-import bg20 from './20.jpg';
-import bg21 from './21.jpg';
+import bg10 from './10.jpg';
 
 
 // export bgs
@@ -35,15 +24,4 @@ export default [
     bg8,
     bg9,
     bg10,
-    bg11,
-    bg12,
-    bg13,
-    bg14,
-    bg15,
-    bg16,
-    bg17,
-    bg18,
-    bg19,
-    bg20,
-    bg21,
 ];
